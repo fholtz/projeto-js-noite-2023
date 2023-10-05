62cd8b560d42f3294b950da15801e929d4d32fc5
amandaendomurayama <amandaendomurayama@hotmail.com>
2023-10-05 19:56:21 -0300

    aula05.10

--- amanda/login.js
+++ amanda/login.js
@@ -1,21 +1,46 @@
 const login = document.querySelector('#login');
-
+const senha = document.querySelector('#senha');
 const botao = document.querySelector('#buscar');
 
-const showData = function (result) {
-    for (const campo in result) {
-        if (document.querySelector('#' + campo)) {
-            document.querySelector('#' + campo).value = result[campo]
-        }
-    }
-}
+// const showData = function (result) {
+//     for (const campo in result) {
+//         if (document.querySelector('#' + campo)) {
+//             document.querySelector('#' + campo).value = result
+//         }
+//     }
+// }
 
 botao.addEventListener('click', function (e) {
-    let search = cep.value.replace('-', '');
+    // let search = login.value.replace('');
+
     const options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default'
     }
 
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=249e3bec', options)
+    // fetch(`http://www.omdbapi.com/?s=${search}&apikey=249e3bec`, options)
+
+    fetch(`./ws/loginJson.json`, options)
+
+        .then(function (response) {
+            response.json()
+                .then(function (dados) {
+
+
+
+                    console.log(dados);
+
+                    for (let index = 0; index < dados.length; index++) {
+                        if (login[index] == login) {
+                            
+
+
+                            if (pastasUnidades[index] < 20) {
+                            }
+                        }
+                        // showData(dados);
+                    })
+        })
+
+})
