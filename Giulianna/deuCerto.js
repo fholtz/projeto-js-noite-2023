ba9219c0ba91e080fb3e61e102f08371bee40c0c
GiuliannaMarcella <md.giulianna@gmail.com>
2023-10-17 19:54:10 -0300

    17-10

--- Giulianna/deuCerto.js
+++ Giulianna/deuCerto.js
@@ -1,14 +1,25 @@
-const filme = document.querySelector('#filme');
+const urlParams = new URLSearchParams(window.location.search);
+const nomeUsuario = urlParams.get("nome")
+document.querySelector('#name').innerHTML=nomeUsuario;
+const filme = document.querySelector('#filme')
 const button = document.querySelector('#pesquisar');
 
-button.addEventListener('click', function (e) { 
 
+button.addEventListener('click', function (e) { 
     const options = {
     method: 'GET',
     mode: 'cors',
     cache: 'default'
     }
+    fetch(`http://www.omdbapi.com/?s=${filme}&apikey=a86f111b`, options)
 
-    fetch('${search}', options)
+    .then(function (response) {
+        response.json()
 
+            .then(function (data) {
+                console.log(data);
+                showdata(data)
+            })
+    })
 })
+
