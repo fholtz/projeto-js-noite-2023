f6655a6616205fc81f22b2cc739ffa136b36cc08
marcelogattermann <marcelo_gattermann@hotmail.com>
2023-10-24 19:53:53 -0300

    jsfim

--- marcelo-giulianna/dadospagina2.js
+++ marcelo-giulianna/dadospagina2.js
@@ -1,9 +1,22 @@
 const urlParams = new URLSearchParams(window.location.search);
 
+
 const nomefilme = urlParams.get("titulo")
 const idfilme = urlParams.get("id")
 console.log(nomefilme)
 console.log(idfilme)
+
+let classificacao = "";
+
+const showData = function (result) {
+    for (const info in result) {
+        if (document.querySelector('#' + info)) {
+            document.querySelector('#' + info).innerHTML += result[info]
+            console.log(result);
+        }
+
+    }
+}
 
 const options = {
     method: 'GET',
@@ -17,8 +30,35 @@ fetch(`http://www.omdbapi.com/?t=${nomefilme}&apikey=3096a4f4`, options)
         response.json()
 
             .then(function (data) {
+                showData(data)
                 console.log(data)
+                switch (data.Rated) {
+                    case "G":
+                        classificacao = "Classificação livre";
+                        break;
 
+                    case "PG":
+                        classificacao = "Orientação dos Responsáveis";
+                        break;
+
+                    case "PG-13":
+                        classificacao = "Classificação 14 anos";
+                        break;
+
+                    case "R":
+                        classificacao = "Classificação 16 anos";
+                        break;
+
+                    case "NC – 17":
+                        classificacao = "Classificação 18 anos";
+                        break;
+                    default:
+                        classificacao = "";
+                        break;
+                }
+
+                document.querySelector("#Rated").innerHTML = classificacao
+                document.querySelector("#capa").innerHTML = `<img src='${data.Poster}'></img></a>`
             })
 
     })
@@ -29,4 +69,5 @@ fetch(`http://www.omdbapi.com/?t=${nomefilme}&apikey=3096a4f4`, options)
     })
 
 
-    
+
+
